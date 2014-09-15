ae60564652c792a695d5d491f416762e0cbfca9b
doodle <doodlehp@gmail.com>
2014-09-15 20:00:19 +0300

    Moved random class to math/Random.h

--- src/bacteroids/BacteroidsGame.cpp
+++ src/bacteroids/BacteroidsGame.cpp
@@ -8,49 +8,12 @@
 
 #include "../math/Math.h"
 #include "../math/Projection.h"
+#include "../math/Random.h"
 
 #include "../Log.h"
 
-#include <random>
-
 namespace bact
 {
-
-    class Random
-    {
-    public:
-        void Seed(uint32_t seed)
-        {
-            m_generator.seed(seed);
-        }
-
-        int GetInt()
-        {
-            std::uniform_int_distribution<int> dis;
-            return dis(m_generator);
-        }
-
-        int GetInt(int a, int b)
-        {
-            std::uniform_int_distribution<int> dis(a, b);
-            return dis(m_generator);
-        }
-
-        double GetReal()
-        {
-            std::uniform_real_distribution<double> dis;
-            return dis(m_generator);
-        }
-
-        double GetReal(double a, double b)
-        {
-            std::uniform_real_distribution<double> dis(a, b);
-            return dis(m_generator);
-        }
-
-    private:
-        std::mt19937 m_generator;
-    };
 
     using namespace rob;
 
@@ -113,8 +76,6 @@ namespace bact
         MicroTicker m_ticker;
         VirtualTime m_time;
         Random m_random;
-
-
 
         float radius;
     };
